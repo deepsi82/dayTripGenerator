064ac0c076d5c78d63874f1a9ac6d5ec2cfa759f
deepsi82 <deepsi82@gmail.com>
2021-03-16 14:42:00 -0500

    second commit

---
+++ dayTripGene.js
@@ -0,0 +1,4 @@
+let destination = prompt("please enter the destination");
+let restaurent = prompt("please enter the restaurent name");
+let modeOfTrans = prompt("please enter the mode of transportation");
+let formOfEntertain = prompt("please enter the form of entertainment");
