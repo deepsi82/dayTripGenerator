d6eba25e5b13236c147faab851067897473782c3
deepsi82 <deepsi82@gmail.com>
2021-03-17 12:19:28 -0500

    forth commit

--- dayTripGene.js
+++ dayTripGene.js
@@ -1,32 +1,32 @@
 "use stirct";
-let destination = ['Milwaukee', 'chicago', 'Austin'];
-var randomDestination = destination[Math.floor(Math.random()*destination.length)];
-console.log(randomDestination);
-let restaurent = ['Olive Garden', 'IHop', 'pandaexpress'];
-var randomres = restaurent[Math.floor(Math.random()*restaurent.length)];
-console.log(randomres);
-let modeOfTrans = ['Bus', 'car', 'train'];
-var randomTrans = modeOfTrans[Math.floor(Math.random()*modeOfTrans.length)];
-console.log(randomTrans);
-let entertain = ['Tv', 'movie', 'videoGame'];
-var randomEnter = entertain[Math.floor(Math.random()*entertain.length)];
-console.log(randomEnter);
-let myDayTrip = confirm("press a button!");
-if(myDayTrip === true){
-    console.log("complete");
+let destination = ['Milwaukee', 'chicago', 'Austin', 'Madison', 'GreenBay'];
+let restaurent = ['Olive Garden', 'IHop', 'pandaexpress', 'Qdoba'];
+let modeOfTrans = ['Bus', 'car', 'train', 'airTravel'];
+let entertain = ['Tv', 'movie', 'videoGame', 'music'];
+
+
+
+function randomTrip(){
+    let randomDestination = destination[Math.floor(Math.random()*destination.length)];
+    console.log(randomDestination);
+    let randomres = restaurent[Math.floor(Math.random()*restaurent.length)];
+    console.log(randomres);
+    let randomTrans = modeOfTrans[Math.floor(Math.random()*modeOfTrans.length)];
+    console.log(randomTrans);
+    let randomEntertain = entertain[Math.floor(Math.random()*entertain.length)];
+    console.log(randomEntertain);
+    return randomDestination + " " + randomres + " " + randomTrans + " " + randomEntertain;
 }
 
 
+let myDayTrip = confirm("press okay buton if you like the random trip!");
+if(myDayTrip === true){
+    alert(randomTrip());
+    console.log("complete");
+}
+else{
+    console.log("please regenerate the random trip");
+}
 
-//let myDayTrip = ['destination', 'restaurent', 'modeOfTransport', 'entertain'];
-//let myDayTrip = [];
-//myDayTrip[0] = prompt("please enter the destination");//MilwaukeeZoo"
-//myDayTrip[1] = prompt("please enter the restaurent");//OliveGarden";
-//myDayTrip[2] = prompt("please enter the mode of transport");//"car";
-//myDayTrip[3] = prompt("please enter the entertain");//"movies";
-//let randomSele = confirm("press a button!");
-//if(randomSelect == ture){
-    //console.log("complete");
-//}
-
-
+    
+    
